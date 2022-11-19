608cddff05462e9553c7a44759d51119b5e7b175
ALAN JOSE <alanjose04231120@gmail.com>
2022-11-18 19:06:53 -0500

    fixed 4/4 tests

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,9 +1,8 @@
 const Intern = require("../lib/Employee");
 
 test("Can create a new Employee?",() => {
-const t1 = new Intern('Alan', 123, 'alan@email.com','Trickum Middle');
+const t1 = new Intern('Alan', 123, 'alan@email.com');
 expect(t1.name).toBe('Alan');
 expect(t1.email).toBe('alan@email.com');
 expect(t1.id).toBe(123);
-expect(t1.sch).toBe(undefined);
 });
