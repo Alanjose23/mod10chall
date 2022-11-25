7e777fe0c5e1d49d8c57aef7b2d1cf33eff316cd
ALAN JOSE <alanjose04231120@gmail.com>
2022-11-25 18:26:11 -0500

    moved package json into main

---
+++ createpage.js
@@ -0,0 +1,70 @@
+
+let finalhtml = [];
+
+const engineer = (dataen) =>{
+return `<div class="card">
+<div class="container">
+  <h4><b>${dataen.name}</b></h4>
+  <p>${dataen.id}}</p>
+  <p>${dataen.email}</p>
+  <p>${dataen.git}</p>
+</div>
+</div>`
+}
+const intern = (dataen) =>{
+    return `<div class="card">
+    <div class="container">
+      <h4><b>${dataen.name}</b></h4>
+      <p>${dataen.id}}</p>
+      <p>${dataen.email}</p>
+      <p>${dataen.git}</p>
+    </div>
+    </div>`
+    }
+const Manager = (dataen) =>{
+        return `<div class="card">
+        <div class="container">
+          <h4><b>${dataen.name}</b></h4>
+          <p>${dataen.id}}</p>
+          <p>${dataen.email}</p>
+          <p>${dataen.git}</p>
+        </div>
+        </div>`
+        }
+const totalp = (final) => {
+    return `<!doctype html>
+    <html>
+    <head>
+    <title>Team Page</title>
+    <meta name="description" content="Our first page">
+    <meta name="keywords" content="html tutorial template">
+    </head>
+    <body>
+    ${pagef(final)}
+    </body>
+    </html>`
+}
+
+// create added html for loop
+function pagef(lit){
+
+let htmlc = [];
+for(i = 0; i > lit; i++){
+if(lit.findRole() === 'Manager'){
+  htmlc.push(Manager(lit[i]))
+}
+else if(lit.findRole() === 'Engineer'){
+  htmlc.push(engineer(lit[i]))
+}
+else {
+    htmlc.push(intern(lit[i]))
+}}
+finalhtml = htmlc.join('');
+return totalp(finalhtml);
+
+}
+
+
+
+
+module.exports = { totalp };
